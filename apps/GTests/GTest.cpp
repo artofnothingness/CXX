b6ef3a3de80210e90a037dd551d15fec2580892c
alex <a.budyakov@fastsense.tech>
2021-02-10 15:00:54 +0300

    add catch2 example

--- apps/GTests/GTest.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(testMath, myCubeTest) {
-  EXPECT_EQ(1000, 1000);
-  EXPECT_EQ(100, 1000);
-}
---
+++ tests/GoogleTest/simple.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+TEST(testMath, myCubeTest) {
+  EXPECT_EQ(1000, 1000);
+  EXPECT_EQ(1000, 1000);
+  EXPECT_EQ(2000, 2000);
+}
